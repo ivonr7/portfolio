e69503a65b9271a7479074d9621dfa3d6f2810c2
ivonr7 <imv@sfu.ca>
2026-01-28 14:32:48 -0800

    replacing with jekyll site

--- JS/closeNav().js
+++
@@ -1,9 +0,0 @@
-function closeNav(){
-    let links = document.getElementById("Links");
-    if(links.style.display == "grid"){
-        links.style.display="none";
-    }
-    else{
-        links.style.display="grid";
-    }
-}
